3b94e3ec447ff6c695b95d6fafa53df303c96176
diffamr <44928835+diffamr@users.noreply.github.com>
2020-07-21 21:21:58 +0700

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.587376,115.181758";
+$header[] = "X-Location: -5.398223,105.264362";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
